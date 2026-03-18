b544e5c897ab9b961d40386c600c309a1e497481
Roman Maydana <ro.28745.maydana@gmail.com>
2026-03-18 15:40:43 -0400

    feat: Implement dynamic display of user's favorite pets, including loading indicators and an empty state message.

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,9 +1,33 @@
 import { DashboardTitle } from '@/components/Dashboard/DashboardTitle'
+import { useFavoriteStore } from '@/store/favorites'
+import { useEffect, useState } from 'react'
+import { type Pet } from '@/types.d'
+import { getFavorites } from '@/services/users'
+import { PetItem } from '@/components/PetItem'
+import { Loading } from '@/components/Loading'
 
 export function FavoritesPage () {
+  const count = useFavoriteStore(state => state.favoriteIds.length)
+  const [pets, setPets] = useState<Pet[]>([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    getFavorites({ offset: 0, limit: 20 })
+      .then((response) => {
+        setPets(response.data)
+        setLoading(false)
+      })
+  }, [])
   return (
     <>
-      <DashboardTitle title='Favorites' description='Manage and track the status of your current pet adoption request.' />
+      <DashboardTitle title='My Favorites' description={`You have ${count} pets in your wishlist awaiting a forever home.`} />
+      {loading && <Loading />}
+      {!loading && pets.length === 0 && <p className='text-gray-500 mt-1 font-light'>No favorites found</p>}
+      <section className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-6'>
+        {pets.map((pet, index) => (
+          <PetItem key={pet.id} pet={pet} index={index} />
+        ))}
+      </section>
     </>
   )
 }
